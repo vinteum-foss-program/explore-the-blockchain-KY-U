31b97bc597e0218df7d2d2751bd922c4a6a4cafb
KYU <uscaio@estudante.ufscar.br>
2024-11-24 20:37:52 -0300

    add: question 7 added

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,31 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+
+hash=$(bitcoin-cli getblockhash 123321)
+block=$(bitcoin-cli getblock $hash 2)
+
+#Cria os comandos a serem rodados
+echo "$block" | jq -r '.tx[] | .txid as $txid | .vout[] | "bitcoin-cli gettxout \($txid) \(.n) | jq -r \".scriptPubKey.address\""' | while read -r cmd; do
+  result=$(eval "$cmd") # Executa o comando e armazena o resultado
+  if [[ -n "$result" ]]; then # Verifica se o resultado não é nulo
+    echo -n $result
+    break # Encerra o loop
+  fi
+done
+
+#nem isso funciona, que tristeza
+#for tx in $(echo "$block" | jq -r '.tx[].txid'); do
+#  tx=$(echo "$tx" | tr -d '\n')
+#  txout=$(bitcoin-cli gettxout "$tx" 0)
+#  echo $txout
+#done
+
+#for tx in $(echo "$block" | jq -r '.tx[].txid'); do
+#  for vout_index in $(echo "$block" | jq -r --arg tx "$tx" '.tx[] | select(.txid == $tx) | .vout[].n'); do
+#    txout=$(bitcoin-cli gettxout "$tx" $vout_index)
+#    echo $txout
+#    if [ "$txout" != "null" ]; then
+#       echo "$txout" | jq -r '.scriptPubKey.address'
+#      break
+#    fi
+#  done
+#done
